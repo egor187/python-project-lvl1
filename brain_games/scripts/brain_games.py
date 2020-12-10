30c8fd198d157839f7eac7faf806b1b9550c7d3f
Егор <oegor8194@gmail.com>
2020-12-10 17:46:51 +0300

    third lesson done

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
-def main():
+
+import brain_games.cli
+
+def greet():
     print('Welcome to the Brain Games!')
+
+def main():
+    greet()
+    brain_games.cli.welcome_user()
+
 
 if __name__ == '__main__':
     main()
